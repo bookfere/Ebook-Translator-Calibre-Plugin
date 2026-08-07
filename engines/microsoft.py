d56b4d4a1dd95521ea2772e8eab8a6b9780e1f28
bookfere <bookfere@gmail.com>
2026-08-07 12:02:15 +0800

    fix(engine): Update Microsoft Edge translate. fixed #601

--- engines/microsoft.py
+++ engines/microsoft.py
@@ -20,38 +20,14 @@ class MicrosoftEdgeTranslate(Base):
     alias = 'Microsoft Edge (Free)'
     free = True
     lang_codes = Base.load_lang_codes(microsoft)
-    endpoint = 'https://api-edge.cognitive.microsofttranslator.com/translate'
+    endpoint = 'https://edge.microsoft.com/translate/translatetext'
     need_api_key = False
     access_info = None
 
-    def _parse_jwt(self, token):
-        parts = token.split(".")
-        if len(parts) <= 1:
-            raise Exception(_('Failed get APP key due to an invalid Token.'))
-        base64_url = parts[1]
-        if not base64_url:
-            raise Exception(
-                _('Failed get APP key due to and invalid Base64 URL.'))
-        base64_url = base64_url.replace('-', '+').replace('_', '/')
-        json_payload = base64.b64decode(base64_url + '===').decode('utf-8')
-        parsed = json.loads(json_payload)
-        expired_date = datetime.fromtimestamp(parsed['exp'])
-        return {'Token': token, 'Expire': expired_date}
-
-    def _get_app_key(self):
-        if not self.access_info or datetime.now() > self.access_info['Expire']:
-            auth_url = 'https://edge.microsoft.com/translate/auth'
-            app_key = request(auth_url, method='GET')
-            self.access_info = self._parse_jwt(app_key)
-        else:
-            app_key = self.access_info['Token']
-        return app_key
-
     def get_endpoint(self):
         query = {
+            'isEnterpriseClient': False,
             'to': self._get_target_code(),
-            'api-version': '3.0',
-            'includeSentenceLength': True,
         }
         if not self._is_auto_lang():
             query['from'] = self._get_source_code()
@@ -60,11 +36,10 @@ class MicrosoftEdgeTranslate(Base):
     def get_headers(self):
         return {
             'Content-Type': 'application/json',
-            'authorization': 'Bearer %s' % self._get_app_key()
         }
 
     def get_body(self, text):
-        return json.dumps([{'text': text}])
+        return json.dumps([text])
 
     def get_result(self, response):
         return json.loads(response)[0]['translations'][0]['text']
